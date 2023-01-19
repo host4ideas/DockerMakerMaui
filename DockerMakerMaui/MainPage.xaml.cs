84bbe911a6d6452883818762aa00bd9ac3d22865
host4ideas <felixmb99@hotmail.es>
2023-01-19 08:55:21 +0100

    adding docker dotnet client library

--- DockerMakerMaui/MainPage.xaml.cs
+++ DockerMakerMaui/MainPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace DockerMakerMaui;
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace DockerMakerMaui;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+    private readonly DockerClient client;
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
 	}
@@ -20,5 +24,37 @@ public partial class MainPage : ContentPage
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
+
+    /// <summary>
+    /// Method <c>Index</c> executed on view creation.
+    /// </summary>
+    private void LoadDocker()
+    {
+        Boolean error = false;
+        string message;
+        IList<ImagesListResponse> imagesResponse;
+        IList<ContainerListResponse> containersResponse;
+
+        try
+        {
+            // try to ping Doccker daemon through Docker Client --> raises DockerApiException
+            client.System.PingAsync().Wait();
+
+            message = "Docker se está ejecutando con normalidad";
+
+            // Obtain the list of images available on the Docker server
+            imagesResponse = client.Images.ListImagesAsync(new ImagesListParameters()).Result;
+
+            // Obtain the list of containers on the Docker server
+            containersResponse = client.Containers.ListContainersAsync(new ContainersListParameters
+            {
+                All = true
+            }).Result;
+        }
+        catch (Exception ex)
+        {
+            message = $"No se pudo conectar al Docker daemon. Compruebe que Docker se está ejecutando con normalidad:<br />{ex.Message}";
+        }
+    }
 }
 
--- DockerMakerMaui/MauiProgram.cs
+++ DockerMakerMaui/MauiProgram.cs
@@ -1,4 +1,7 @@
-﻿namespace DockerMakerMaui;
+﻿using System.Runtime.InteropServices;
+using Docker.DotNet;
+
+namespace DockerMakerMaui;
 
 public static class MauiProgram
 {
@@ -13,6 +16,30 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
-		return builder.Build();
+        var IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        var IsLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+        string DockerApiUri()
+        {
+            if (IsWindows)
+                return "npipe://./pipe/docker_engine";
+
+            if (IsLinux)
+                return "unix:///var/run/docker.sock";
+
+            throw new Exception(
+                "Was unable to determine what OS this is running on, does not appear to be Windows or Linux!?");
+        }
+
+        // Agregar el cliente de Docker como un servicio
+        DockerClient client = new DockerClientConfiguration(
+            new Uri(DockerApiUri()))
+             .CreateClient();
+
+        System.Diagnostics.Debug.WriteLine(client);
+
+        builder.Services.AddSingleton(client);
+
+        return builder.Build();
 	}
 }
